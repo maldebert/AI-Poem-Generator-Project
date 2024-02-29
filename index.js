6f12110551720c6fc6b7de0307ad74c5c58c0497
Marie <ma-aldebert@hotmail.fr>
2024-02-29 09:44:36 +0000

    improve user exp and added styling

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#instructions");
   let apiKey = "ef7417d18a5fa20et4ba9o259132407e";
   let context =
-    "You are a romantic poem expert and you love to write short poems. Your mission is to generate a four line poem in basic html and seperate each line with a <br />. Make sure to follow the user instructions below. Don't add quotes to the poem or show html code.";
+    "You are a romantic poem expert and you love to write short poems. Your mission is to generate a four line poem in basic html and seperate each line with a <br />. Make sure to follow the user instructions below. Don't add quotes to the poem and don't show html code.";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">🌻🌹🌸🪻Generating a poem about ${instructionsInput.value} 🌼🌷💐🌺</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
